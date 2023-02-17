e900cab0d085bd75a8a37a92598cc492ad280d87
Glenn L. Jenkins <GLJenkins@cardiffmet.ac.uk>
2023-02-17 12:59:23 +0000

    Adding bullet pool

---
+++ SpaceGame/src/BulletPool.cpp
@@ -0,0 +1,41 @@
+#include "Bullet.h"
+
+#include <glm/glm.hpp> // normalise
+#include <glm/gtx/fast_square_root.hpp> // fast normalise 
+
+const float Bullet::MAX_VELOCITY = 1.0f;
+
+Bullet::Bullet()
+{
+	rotation = 0.0f;
+	position = { 0.0f, 0.0f, 0.0f };
+	size = { 1.0f, 1.0f };
+	velocity = { 0.0f,0.0f,0.0f };
+}
+
+Bullet::~Bullet()
+{
+
+}
+void Bullet::init(glm::vec3 direction)
+{
+	m_CheckerboardTexture = Hazel::Texture2D::Create("assets/textures/Checkerboard.png");
+
+	velocity = glm::fastNormalize(direction);
+	velocity *= Bullet::MAX_VELOCITY;
+}
+
+void Bullet::draw()
+{
+	Hazel::Renderer2D::DrawRotatedQuad(position, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 1.0f);
+}
+
+void Bullet::update(Hazel::Timestep ts)
+{
+	HZ_PROFILE_FUNCTION();
+
+	velocity *= Bullet::MAX_VELOCITY;
+
+	position += velocity *= ts.GetSeconds();
+
+}
---
+++ SpaceGame/src/BulletPool.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Hazel.h"
+
+class Bullet
+{
+private:
+	Hazel::Ref<Hazel::Texture2D> m_CheckerboardTexture;
+	float rotation;
+	glm::vec3 position;
+	glm::vec3 velocity;
+	glm::vec2 size;
+	static const float MAX_VELOCITY;
+	bool active;
+public:
+	Bullet();
+	~Bullet();
+
+	void init();
+	void draw();
+	void update(Hazel::Timestep ts);
+
+	inline bool isActive() { return active; };
+	inline void setActive(bool a) { active = a; };
+};
+
